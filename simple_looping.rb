0a12eff67829078cf84d4dd935f5285f2c992016
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-14 18:24:29 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -54,6 +54,7 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for (0...number_of_times) do
-    puts phrase
+      puts phrase
+    end
   end
 end
